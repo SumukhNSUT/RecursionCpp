eab9e2235e6a1c9c9024b73722733114ccd50b4f
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2025-03-15 00:10:04 +0530

    2

---
+++ 2.cpp
@@ -0,0 +1,58 @@
+// // print name 5 times
+// #include <iostream>
+// using namespace std;
+
+// void print5(int cnt) {
+//     if (cnt > 5) {
+//         return;
+//     }
+//     else {
+//         cout << "NAME" << endl;
+//         cnt++;
+//         print5(cnt);
+//     }
+
+// }
+
+// int main() {
+//     print5(1);
+//     return 0;
+// }
+
+// // print linearly from n to 1
+// #include <iostream>
+// using namespace std;
+
+// void print(int n) {
+//     if (n == 0) {
+//         return;
+//     }
+//     cout << n << endl;
+//     print(n - 1);
+// }
+
+// int main() {
+//     print(5);
+//     return 0;
+// }
+
+// // print linearly from 1 to n
+// #include <iostream>
+// using namespace std;
+
+// void print(int n, int cnt) {
+
+//     if (cnt > n) {
+//         return;
+//     }
+
+//     cout << cnt << endl;
+//     cnt++;
+//     print(n, cnt);
+// }
+
+// int main() {
+//     print(5, 1);
+//     return 0;
+// }
+
