30eb93a455f35581883074eb0b12729fb9122fa0
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2025-03-21 17:35:52 +0530

    5

---
+++ 5.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// using namespace std;
+// // nth fibonacci number
+// int fib(int n) {
+//     if (n <= 1) {
+//         return n;
+//     }
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+// int main() {
+//     int n = 6;
+//     for (int i = 1; i <= n; i++) {
+//         cout << fib(i) << " ";
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
